5ca3bdc20a57702b7d95fbe738fd3b0db70c1296
Mathias Iden <mathias.iden@knowit.no>
2017-07-07 16:19:25 +0200

    Added starters for login and register

--- meals-api/meals/views.py
+++ meals-api/meals/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
-from django.shortcuts import render
 
-# Create your views here.
+@api_view(["POST"])
+def login(request):
+    print request.data
+    # Get username and password and return token
+    return Response("hei", status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def register(request):
+    print request.data
+    # Register username and password and return token
+    return Response("hei", status=status.HTTP_200_OK)
--- meals-api/mealsapi/settings.py
+++ meals-api/mealsapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'rest_framework',
     'meals'
 ]
 
